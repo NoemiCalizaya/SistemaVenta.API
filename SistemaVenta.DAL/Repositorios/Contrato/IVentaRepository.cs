44932590f8265e40e923662aa00f347a74d3682f
NoemiCalizaya <calizayanoemiisabel@gmail.com>
2026-06-02 21:42:52 -0400

    adding DTOs and Mappers

---
+++ SistemaVenta.DAL/Repositorios/Contrato/IVentaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+	public interface IVentaRepository : IGenericRepository<Venta>
+    {
+		Task<Venta> Registrar(Venta modelo);
+    }
+}
